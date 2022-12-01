c32f960197fe338e11e9e0332f00bd7fe6c6873f
simorgh3196 <simorgh3196@gmail.com>
2022-12-01 17:59:55 +0900

    Add test for decode configuration file

---
+++ Tests/SecretKeysTests/ConfigurationTests.swift
@@ -0,0 +1,80 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Tomoya Hayakawa (github.com/simorgh3196).
+
+import XCTest
+import Yams
+@testable import secret_keys
+
+final class ConfigurationDecoderTests: XCTestCase {
+    var decoder: YAMLDecoder!
+
+    override func setUpWithError() throws {
+        decoder = YAMLDecoder()
+    }
+
+    func testDecodeConfiguration() throws {
+        let configYamlData = """
+        # Select the package manager to use from `spm` or `cocoapods`. (Default: `spm`)
+        packageManager: spm
+
+        # Determine the name of the struct. (Default: `Keys`)
+        namespace: TestKeys
+
+        # Also generate unit tests for accessing keys. (Default: `false`)
+        withUnitTest: true
+
+        # The path of output directory. (Default: `./Dependencies`)
+        outputDirectory: _Keys
+
+        # In addition to environment variables, properties files can be read.
+        sources:
+          - .env
+          - .env.default
+
+        # Mapping variable names to environment variable names.
+        keys:
+          clientID: CLIENT_ID
+          clientSecret: CLIENT_SECRET
+
+        # Set the target according to the environment and other requirements that
+        # you want to use different build modes, etc.
+        targets:
+          - name: SharedSecretKeys
+            namespace: SharedKeys # can override namespace
+          - name: SecretKeysDebug
+          - name: SecretKeysProduction
+            sources: # additional properties files can be specified
+              - .env.production
+            keys: # can override keys
+              clientID: PRODUCTION_CLIENT_ID
+              clientSecret: PRODUCTION_CLIENT_SECRET
+        """.data(using: .utf8)!
+
+        let config = try decoder.decode(Configuration.self, from: configYamlData)
+
+        XCTAssertEqual(config.packageManager, .spm)
+        XCTAssertEqual(config.namespace, "TestKeys")
+        XCTAssertEqual(config.withUnitTest, true)
+        XCTAssertEqual(config.outputDirectory, "_Keys")
+        XCTAssertEqual(config.sources, [
+            ".env",
+            ".env.default",
+        ])
+        XCTAssertEqual(config.keys, [
+            "clientID": "CLIENT_ID",
+            "clientSecret": "CLIENT_SECRET",
+        ])
+        XCTAssertEqual(config.targets.count, 3)
+        XCTAssertEqual(config.targets[0], Configuration.Target(name: "SharedSecretKeys", namespace: "SharedKeys"))
+        XCTAssertEqual(config.targets[1], Configuration.Target(name: "SecretKeysDebug"))
+        XCTAssertEqual(config.targets[2], Configuration.Target(
+            name: "SecretKeysProduction",
+            sources: [".env.production"],
+            keys: [
+                "clientID": "PRODUCTION_CLIENT_ID",
+                "clientSecret": "PRODUCTION_CLIENT_SECRET"
+            ])
+        )
+    }
+}
