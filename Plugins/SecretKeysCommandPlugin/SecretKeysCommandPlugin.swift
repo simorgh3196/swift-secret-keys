f7325d91d7af9edd0074503c98ba5365f8fb7fc6
simorgh3196 <simorgh3196@gmail.com>
2023-01-14 23:11:00 +0900

    Fix command options

--- Plugins/SecretKeysCommandPlugin/SecretKeysCommandPlugin.swift
+++ Plugins/SecretKeysCommandPlugin/SecretKeysCommandPlugin.swift
@@ -11,13 +11,6 @@ struct SecretKeysCommandPlugin: CommandPlugin {
     func performCommand(context: PluginContext, arguments: [String]) throws {
         let secretKeysExecutableURL = URL(fileURLWithPath: try context.tool(named: "secret-keys").path.string)
 
-        // If the `--help` or `-h` flag was passed, print the plugin's help information
-        // and exit.
-        if arguments.contains("--help") || arguments.contains("-h") {
-            print(Self.helpText)
-            return
-        }
-
         let process = try Process.run(secretKeysExecutableURL, arguments: arguments)
         process.waitUntilExit()
 
@@ -26,16 +19,4 @@ struct SecretKeysCommandPlugin: CommandPlugin {
             Diagnostics.error("SecretKeys project generation failed: \(problem)")
         }
     }
-
-    private static let helpText = """
-        OVERVIEW: Generates a project to access the values of environment variables and properties files.
-        USAGE: secret-keys generate [--config <config>] [--verbose]
-
-        OPTIONS:
-        -c, --config <config>   The path to the configuration file (default: .secretkeys.yml)
-        -v, --verbose           Enables verbose log messages
-        --version               Show the version.
-        -h, --help              Show help information.
-
-        """
 }
